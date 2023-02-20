7098fdbed96183509da10ace1eba579e71f5fc0d
Verito123 <verito_326@outlook.es>
2023-02-20 12:59:20 -0600

    agregue un comentario

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//Soy un comentario
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
